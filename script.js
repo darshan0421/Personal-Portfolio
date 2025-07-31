6c9d9dff7310f1c5e7ea969e2bc6d51a3e8e792e
Mr.Darshan Dakhane <168203751+darshan0421@users.noreply.github.com>
2025-07-31 10:01:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+ScrollReveal().reveal('.hero-left', { origin: 'left', distance: '50px', duration: 1000, delay: 300 });
+ScrollReveal().reveal('.hero-right', { origin: 'right', distance: '50px', duration: 1000, delay: 500 });
+ScrollReveal().reveal('.navbar', { origin: 'top', distance: '20px', duration: 800, delay: 200 });
+
+ScrollReveal().reveal('.about-img', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  delay: 200
+});
+ScrollReveal().reveal('.about-content', {
+  origin: 'right',
+  distance: '50px',
+  duration: 1000,
+  delay: 300
+});
+
+
+ScrollReveal().reveal('.contact-section', {
+  origin: 'bottom',
+  distance: '60px',
+  duration: 1000,
+  delay: 300,
+});
